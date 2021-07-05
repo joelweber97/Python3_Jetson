8725a48e433be1cb4293a8a0bc1cf066fdf9d615
JW_Jetson <joel.weber97@gmail.com>
2021-07-05 18:54:36 -0400

    pep8 formatting changes

--- OpenCV-1.py
+++ OpenCV-1.py
@@ -1,15 +1,15 @@
 import cv2
-print(cv2.__version__)
 import numpy as np
+print(cv2.__version__)
 print(np.__version__)
 
 
-#webcam
+# webcam
 cam = cv2.VideoCapture('/dev/video1')
 
-#RPi Camera
-#camSet = 'nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=3264, height=2464, framerate=21/1, format=NV12 ! nvvidconv flip-method=2 ! video/x-raw, width=800, height=600, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink drop=1'
-#cam = cv2.VideoCapture(camSet)
+# RPi Camera
+# camSet = 'nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=3264, height=2464, framerate=21/1, format=NV12 ! nvvidconv flip-method=2 ! video/x-raw, width=800, height=600, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink drop=1'
+# cam = cv2.VideoCapture(camSet)
 
 
 while True:
@@ -17,7 +17,7 @@ while True:
     fps = cam.get(cv2.CAP_PROP_FPS)
     print(fps)
     cv2.imshow('frame', frame)
-    cv2.moveWindow('frame', 0,0)
+    cv2.moveWindow('frame', 0 , 0)
     if cv2.waitKey(1) == ord('q'):
         break
 
